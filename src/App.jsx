1b42c8a9d4a9eeff1cc1ac7469a2804c4307d91d
Natalia Lopez <natigab.24@gmail.com>
2024-01-09 01:34:47 -0300

    corrijo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Informacion from "./componentes/Informacion";
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
+  const anioActual = 2023;
   return (
   <section>
     <h1 className="display-3 text-center">Proyecto de react</h1>
-    <Informacion></Informacion>
+    <Informacion comision='c76i' anioActualProps={anioActual}></Informacion>
   </section>
   );
 }
--- src/componentes/Informacion.jsx
+++ src/componentes/Informacion.jsx
@@ -1,12 +1,13 @@
-const Informacion = () => {
+const Informacion = (props) => {
     //aqui va la lógica del componente
     return (
         //aqui pueder ir un poco de lógica
         <>
-        <h2>Conceptos de react</h2>
+        <h2>Conceptos de react {props.comision}</h2>
         <section>
            {/*aqui va todo el maquetado del componente*/} 
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa ut, dolorum velit sequi voluptate quam fugit aut molestiae numquam iusto natus temporibus beatae nemo omnis modi, minus ipsum quae accusantium.</p>
+        comision: {props.anioActualProps}
         </section>
         </>
     );
