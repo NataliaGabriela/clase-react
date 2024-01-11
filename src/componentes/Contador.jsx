5e31d079ad05e5923a1a82ffe7a82903a2803bb4
Natalia Lopez <natigab.24@gmail.com>
2024-01-11 01:01:12 -0300

    agregamos la funcion setNumero para actualizar el valor del state

--- src/componentes/Contador.jsx
+++ src/componentes/Contador.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 const Contador = () => {
-    const [numero, setNumero] = useState(10)
+    const [numero, setNumero] = useState(1)
   //let numero = 10;
+  
   return (
     <article className="text-center">
       <h2>Contador</h2>
       <p>{numero}</p>
       <button
         className="btn btn-primary"
-        onClick={() => {
-          numero++;
-          console.log(numero);
-        }}
+        onClick={() =>  setNumero(numero + 1)}
       >
         +1
       </button>
+      {/*todo:agreguen un boton para decrementar en -1 el valor del state */}
     </article>
   );
 };
